4a00b79888bb6372e5d3fc54c1d0a68376669f14
Kundanm881 <kundanm881@gmail.com>
2025-05-16 15:36:01 +0530

    test

---
+++ main.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./routers/user_router");
+
+const http = require('http');
+
+
+
+const app = express();
+const port = 3000;
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const { on } = require("events");
+const io = new Server(server);
+
+
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+const { timeStamp } = require("console");
+const messageModel = new mongoose.Schema({
+   message: String,
+}, { versionKey: false, timestamps: true });
+
+const msg = mongoose.model("messages", messageModel);
+
+
+
+
+server.listen(port, () => {
+   const address = server.address();
+   console.log("Server Started on " + address.port);
+})
+
+
+io.on('connection', async (socket) => {
+   console.log('a user connected ' + socket.id);
+
+
+
+   // init list of users
+
+   socket.on("init", async () => {
+      const userData = await getMessgaes();
+      socket.emit("users", userData);
+   });
+
+   socket.on("add-message", async (mmg) => {
+      await msg.create({ message: mmg });
+      const userData = await getMessgaes();
+      socket.emit("users", userData);
+   });
+
+   socket.on('disconnect', () => {
+      console.log('user disconnected ' + socket.id);
+   });
+});
+
+async function getMessgaes() {
+
+   return await msg.find({});
+};
+
+app.use("/user", router);
+
+
+
+
+
